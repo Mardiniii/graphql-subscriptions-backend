3b844b29a5eb70ce255c941326b5f58e44495f07
Sebastian Zapata Mardini <sezama03@hotmail.com>
2020-08-21 11:31:26 -0400

    Add a callback in the `Link` model to update subscriptions

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,9 @@
 class Link < ApplicationRecord
+  after_create :notify_creation_to_subscribers
+
+  private
+
+  def notify_creation_to_subscribers
+    GraphqlRailsApiSchema.subscriptions.trigger(:new_link, {}, self)
+  end
 end
